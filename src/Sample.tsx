d0d5295f2662016f814ba1fbd710d1579a3ee4b1
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-09 21:53:50 +0530

    done

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -3,19 +3,54 @@ import * as React from 'react';
 export interface SampleProps {}
 
 const Sample: React.SFC<SampleProps> = (props) => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [index, setIndex] = React.useState(1);
+  const [count, setCount] = React.useState(0);
+
+  const getCubeFn = React.useCallback(() => {
+    return index * index * index;
+  }, [index]);
+
+  //   Normal code:
+  //   const getCubeFn = () => {
+  //     return index * index * index;
+  //   };
+
+  //    This value is cached and the function doesn't run
+  //    on every render (except for its dependencies)
+  const getCubeValue = React.useMemo(
+    () => ({
+      value: index * index * index,
+    }),
+    [index]
+  );
+
+  //   Normal code:
+  //   const computeCube = () => ({
+  //     value: index * index * index,
+  //   });
+
+  //   const getCubeValue = computeCube();
 
   React.useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.value = 'Good to see you here!';
-      inputRef.current.select();
-    }
-  }, []);
+    console.log('getCubeFn created');
+  }, [getCubeFn]);
+
+  React.useEffect(() => {
+    console.log('getCubeValue created');
+  }, [getCubeValue]);
 
   return (
     <>
-      <h4>Sample Component</h4>
-      <input type='text' ref={inputRef} />
+      <h4>
+        Sample Component: {getCubeFn()} / {getCubeValue.value}
+      </h4>
+
+      <p>Count: {count}</p>
+
+      <hr />
+
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <button onClick={() => setIndex(index + 1)}>Increment Index</button>
     </>
   );
 };
