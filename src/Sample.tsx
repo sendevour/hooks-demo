c6686985c857224a085f4863fb6bc355f569faeb
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-09 21:07:10 +0530

    done

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -2,40 +2,20 @@ import * as React from 'react';
 
 export interface SampleProps {}
 
-const reducer = (state: any, action: any) => {
-  switch (action.type) {
-    case 'add':
-      return [...state, action.payload];
-    case 'remove':
-      return [
-        ...state.slice(0, action.index),
-        ...state.slice(action.index + 1),
-      ];
-    default:
-      throw new Error();
-  }
-};
-
 const Sample: React.SFC<SampleProps> = (props) => {
-  const [state, dispatch] = React.useReducer(reducer, [
-    'Mumbai',
-    'Bengaluru',
-    'Delhi',
-  ]);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = 'Good to see you here!';
+      inputRef.current.select();
+    }
+  }, []);
 
   return (
     <>
       <h4>Sample Component</h4>
-
-      <ul>
-        {state.map((c, i) => {
-          return <li key={i}>{c}</li>;
-        })}
-      </ul>
-
-      <button onClick={() => dispatch({ type: 'add', payload: 'Ahmadabad' })}>
-        Add City
-      </button>
+      <input type='text' ref={inputRef} />
     </>
   );
 };
